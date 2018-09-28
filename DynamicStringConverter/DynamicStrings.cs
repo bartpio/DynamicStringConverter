0652562b71de86f65d508964c024405ff9cc38a5
Bart Piotrowski <31428082+bartpio@users.noreply.github.com>
2018-09-27 22:51:45 -0400

    bugfix, add some castability to DynamicStrings, note no nullable support

--- DynamicStringConverter/DynamicStrings.cs
+++ DynamicStringConverter/DynamicStrings.cs
@@ -5,17 +5,32 @@ using System.Text;
 using System.Linq;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.Collections;
 
 namespace DynamicStringConverter
 {
-    public class DynamicStrings : DynamicObject
+    /// <summary>
+    /// Dynamic Strings holder
+    /// Represents (string, string) key value pairs, with dynamic auto-convert access to the values
+    /// </summary>
+    public class DynamicStrings : DynamicObject, IDictionary<string, string>
     {
+        /// <summary>
+        /// main map storage
+        /// </summary>
         private ReadOnlyDictionary<string, DynamicString> Map { get; }
+
+        /// <summary>
+        /// Pull a stringform rep for interface adaptation
+        /// </summary>
+        private ReadOnlyDictionary<string, string> Stringform => new ReadOnlyDictionary<string, string>(Map.ToDictionary(x => x.Key, x => x.Value?.ToString()));
 
         /// <summary>
         /// optional custom type conv
         /// </summary>
         private ReadOnlyCollection<TypeConverter> CustomTypeConverters { get; }
+
+      
 
         /// <summary>
         /// cons, given enumerable
@@ -33,7 +48,8 @@ namespace DynamicStringConverter
 
             comparer = comparer ?? StringComparer.OrdinalIgnoreCase;
             CustomTypeConverters = tc?.Where(x => x.CanConvertFrom(typeof(string))).ToList().AsReadOnly();
-            Map = new ReadOnlyDictionary<string, DynamicString>(strings.ToDictionary(x => x.Key, x => new DynamicString(x.Value, CustomTypeConverters), comparer));
+            //for nulls the val will be direct null.
+            Map = new ReadOnlyDictionary<string, DynamicString>(strings.ToDictionary(x => x.Key, x => x.Value != null ? new DynamicString(x.Value, CustomTypeConverters) : null, comparer));
         }
 
         /// <summary>
@@ -43,7 +59,8 @@ namespace DynamicStringConverter
         /// <param name="tc">custom type converter; if supplied, we will try these converters prior to trying Convert.ChangeType, in order supplied.
         /// Supplied converters must support converting FROM string. Any converters not supporting this will be ignored.
         /// </param>
-        public DynamicStrings(IEnumerable<(string, string)> strings, IEqualityComparer<string> comparer = null, IEnumerable<TypeConverter> tc = null) : this(AssertValid(strings).Select(x => new KeyValuePair<string, string>(x.Item1, x.Item2)), comparer, tc)
+        public DynamicStrings(IEnumerable<(string, string)> strings, IEqualityComparer<string> comparer = null, IEnumerable<TypeConverter> tc = null) 
+            : this(AssertValid(strings).Select(x => new KeyValuePair<string, string>(x.Item1, x.Item2)), comparer, tc)
         {
         }
 
@@ -73,6 +90,7 @@ namespace DynamicStringConverter
             if (Map.TryGetValue(binder.Name, out var sresult))
             {
                 result = sresult;
+                //result = (sresult.ToString() != null) ? sresult : null;
                 return true;
             }
             else
@@ -104,5 +122,83 @@ namespace DynamicStringConverter
         {
             throw new NotSupportedException("DynamicStrings is immutable");
         }
+
+        /// <summary>
+        /// Get dynamic member names.
+        /// </summary>
+        /// <returns></returns>
+        public override IEnumerable<string> GetDynamicMemberNames()
+        {
+            return Map.Keys.ToList().AsReadOnly();
+        }
+
+
+        #region IDictionary impl
+
+        public ICollection<string> Keys => ((IDictionary<string, string>)Stringform).Keys;
+
+        public ICollection<string> Values => ((IDictionary<string, string>)Stringform).Values;
+
+        public int Count => ((IDictionary<string, string>)Stringform).Count;
+
+        public bool IsReadOnly => ((IDictionary<string, string>)Stringform).IsReadOnly;
+
+        public string this[string key] { get => ((IDictionary<string, string>)Stringform)[key]; set => ((IDictionary<string, string>)Stringform)[key] = value; }
+
+        public void Add(string key, string value)
+        {
+            ((IDictionary<string, string>)Stringform).Add(key, value);
+        }
+
+        public bool ContainsKey(string key)
+        {
+            return ((IDictionary<string, string>)Stringform).ContainsKey(key);
+        }
+
+        public bool Remove(string key)
+        {
+            return ((IDictionary<string, string>)Stringform).Remove(key);
+        }
+
+        public bool TryGetValue(string key, out string value)
+        {
+            return ((IDictionary<string, string>)Stringform).TryGetValue(key, out value);
+        }
+
+        public void Add(KeyValuePair<string, string> item)
+        {
+            ((IDictionary<string, string>)Stringform).Add(item);
+        }
+
+        public void Clear()
+        {
+            ((IDictionary<string, string>)Stringform).Clear();
+        }
+
+        public bool Contains(KeyValuePair<string, string> item)
+        {
+            return ((IDictionary<string, string>)Stringform).Contains(item);
+        }
+
+        public void CopyTo(KeyValuePair<string, string>[] array, int arrayIndex)
+        {
+            ((IDictionary<string, string>)Stringform).CopyTo(array, arrayIndex);
+        }
+
+        public bool Remove(KeyValuePair<string, string> item)
+        {
+            return ((IDictionary<string, string>)Stringform).Remove(item);
+        }
+
+        public IEnumerator<KeyValuePair<string, string>> GetEnumerator()
+        {
+            return ((IDictionary<string, string>)Stringform).GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return ((IDictionary<string, string>)Stringform).GetEnumerator();
+        }
+        #endregion
     }
 }
